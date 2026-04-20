07a2e39b4e137682c4ddf8f482f8a9b1984e6c1c
serhii hladchenko <gladseo@gmail.com>
2026-04-20 14:14:46 +0300

    move terraform folder

--- terraform/repo.tf
+++ terraform/repo.tf
@@ -1,5 +1,4 @@
 resource "github_repository" "trivy_operator" {
-  count = var.create_github_repo ? 1 : 0  
   name        = "trivy-operator"
   description = "Infrastructure and automation for Trivy Operator on GKE with BigQuery log synchronization."
   visibility  = "public"
